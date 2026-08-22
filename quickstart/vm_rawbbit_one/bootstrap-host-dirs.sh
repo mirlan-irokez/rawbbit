25542e12d0977fc27ecee46cbf272b70e0b92571
Mirlan Dzhumagulov <mirlan.irokez@gmail.com>
2026-08-22 20:29:48 +0300

    Ansible deployment fix for Rawbbit-one flow

--- quickstart/vm_rawbbit_one/bootstrap-host-dirs.sh
+++ quickstart/vm_rawbbit_one/bootstrap-host-dirs.sh
@@ -8,17 +8,32 @@ if [[ "${EUID}" -ne 0 ]]; then
   exit 1
 fi
 
-dirs=(
+runtime_owner="${RAWBBIT_ONE_RUNTIME_OWNER:-${SUDO_USER:-root}}"
+runtime_group="$(id -gn "${runtime_owner}")"
+
+runtime_dirs=(
   "${ROOT_DIR}/nats/jetstream"
   "${ROOT_DIR}/seaweedfs/master"
   "${ROOT_DIR}/seaweedfs/volume"
   "${ROOT_DIR}/seaweedfs/filerldb2"
+)
+
+root_dirs=(
   "${ROOT_DIR}/caddy/data"
   "${ROOT_DIR}/caddy/config"
 )
 
 echo "Creating Rawbbit one-VM host directories under ${ROOT_DIR}"
-for dir in "${dirs[@]}"; do
+
+for dir in "${runtime_dirs[@]}"; do
+  install -d -m 0755 \
+    -o "${runtime_owner}" \
+    -g "${runtime_group}" \
+    "${dir}"
+  echo "created_or_verified ${dir} owner=${runtime_owner}:${runtime_group}"
+done
+
+for dir in "${root_dirs[@]}"; do
   install -d -m 0755 "${dir}"
   echo "created_or_verified ${dir}"
 done
